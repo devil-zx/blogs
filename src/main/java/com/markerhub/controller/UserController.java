62130b85708b73af8c9fd394e2b24f2b554dfa3e
devil-zx <1976706634@qq.com>
2020-10-04 23:22:23 +0800

    最初の試す4

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2020-10-04
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/index")
+    public Result index(){
+        User user = userService.getById(1L);
+        return  Result.success(user);
+    }
+}
