a5bca8ffd83a51f7cad2f2d422655e35874154ad
zhaoxiang <1976706634@qq.com>
2020-10-04 11:38:42 +0800

    最初の試す

---
+++ src/main/java/com/example/moke/MokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.moke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moke/MokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
